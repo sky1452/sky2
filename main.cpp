559d5b6b6918ad2a06583251b81c0ae7ebaba71a
Ivan <skywoud987@gmail.com>
2024-12-16 21:16:20 +0300

    Первоначальный commit: добавлен main.cpp с пустой функцией main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
-#include <iostream>
 #include "sdt.h"
 
 int main() {
-    int a, b;
-    std::cout << "Enter two integers: ";
-    std::cin >> a >> b;
-    std::cout << "Sum: " << (a + b) << std::endl;
+    
     return 0;
 }
